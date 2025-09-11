ca4722069323db303d8fb874a468f97c1f46d00e
david.lancellotti <david.lancellotti@gmail.com>
2025-09-11 12:32:32 -0400

    Checked off a few "TODO" items from the list

--- Testbed.Console/Program.cs
+++ Testbed.Console/Program.cs
@@ -19,11 +19,8 @@ internal class Program
     //          - Age
     //          - IsTrained
     //          - IsDomesticated
-    //      - Create a few more animal classes
     //      - Add in the ability to show how many "limbs" and the "type" of limbs they have (i.e. 2 wings vs 4 legs)
-    //      - Use vector instead of array for names to avoid having to state how many names there are?
     //      - Create a way to organize the animals by attributes (name, limbs, height, weight, action being taken, etc)
-    //      - List out how many judgemental animals we have
     
     private static void Main(string[] args)
     {
