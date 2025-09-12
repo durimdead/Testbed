21bb596d12c2bb8fe8ee3d6ca1a7d99bde378468
david.lancellotti <david.lancellotti@gmail.com>
2025-09-12 15:39:22 -0400

    Removed unused "using" statements from the MenuHelper

--- Testbed.Common/Helpers/MenuHelper.cs
+++ Testbed.Common/Helpers/MenuHelper.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Testbed.Common.Models.Animals;
-using Testbed.Common.Sorting.AnimalSort.Interfaces;
 using Testbed.Common.Sorting.Common.Interfaces;
-using static Testbed.Common.Enums.AnimalEnums;
 
 namespace Testbed.Common.Helpers
 {
     public static class MenuHelper<TEnum,TType,TSortingInterface> where TEnum : struct, IConvertible
     {
 
+        /// <summary>
+        /// gets a set of stored methods to perform a given type of sorting algorithm
+        /// </summary>
+        /// <returns>A dictionary of Sort types for a given the enum for available sorts for that type and the base type to sort</returns>
         public static Dictionary<TEnum, IItemSort<TType>> InitializeSortingAlgorithms()
         {
             Dictionary<TEnum, IItemSort<TType>> returnValue = new Dictionary<TEnum, IItemSort<TType>>();
