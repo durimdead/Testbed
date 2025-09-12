654f6eec62209045b1c64b62232282adb511855c
david.lancellotti <david.lancellotti@gmail.com>
2025-09-12 13:26:29 -0400

    updated interfaces to have a base interface for sorting that uses a generic (for ease of updating additional future sorting sets

--- Testbed.Common/Sorting/AnimalSort/Interfaces/IAnimalSort.cs
+++ Testbed.Common/Sorting/AnimalSort/Interfaces/IAnimalSort.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Testbed.Common.Models.Animals;
+using Testbed.Common.Sorting.Common.Interfaces;
 using static Testbed.Common.Enums.CommonEnums;
 
 namespace Testbed.Common.Sorting.AnimalSort.Interfaces
 {
-    public interface IAnimalSort
+    public interface IAnimalSort : IItemSort<Animal>
     {
         /// <summary>
         /// Takes a list of animals and returns a sorted list of animals
@@ -16,6 +17,6 @@ namespace Testbed.Common.Sorting.AnimalSort.Interfaces
         /// <param name="animalsToSort">the list of Animals to sort</param>
         /// <param name="sortOrder">The sort order</param>
         /// <returns>A list of sorted animals</returns>
-        public List<Animal> Sort(List<Animal> animalsToSort, SortOrder sortOrder);
+        public new List<Animal> Sort(List<Animal> animalsToSort, SortOrder sortOrder);
     }
 }
---
+++ Testbed.Common/Sorting/Common/Interfaces/IItemSort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testbed.Common.Models.Animals;
+using static Testbed.Common.Enums.CommonEnums;
+
+namespace Testbed.Common.Sorting.Common.Interfaces
+{
+    public interface IItemSort<T>
+    {
+        /// <summary>
+        /// Takes a list of items and returns a sorted list of items
+        /// </summary>
+        /// <param name="itemsToSort">the list of items to sort</param>
+        /// <param name="sortOrder">The sort order</param>
+        /// <returns>A list of sorted items</returns>
+        public List<T> Sort(List<T> itemsToSort, SortOrder sortOrder);
+    }
+}
